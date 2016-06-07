5656d2a911cdd2ee3a7d522f1da35e4d48a01d9c
Guilherme Moreira <guilherme.moreira@locaweb.com.br>
2016-06-07 15:48:12 -0300

    Finished clojure 'bob' exercise

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,8 +1,35 @@
 (ns bob)
+(require '[clojure.string :as string])
+
+(defn empty-question? "Nothing was said."
+  [text]
+  (string/blank? text)
+  )
+
+(defn question? "Is it a question?"
+  [question]
+  (string/ends-with? question "?")
+  )
+
+(defn yelling? "Is he yelling?"
+  [text]
+  (let [
+        upcase-seq (re-seq #"[A-Z]" text)
+        downcase-seq (re-seq #"[a-z]" text)
+        ]
+    (and
+      (empty? downcase-seq)
+      (not (empty? upcase-seq))
+      )
+    )
+  )
 
 (defn response-for "What bob says."
   [question]
-  (case question
-    "" "Fine. Be that way!"
+  (cond
+    (bob/empty-question? question) "Fine. Be that way!"
+    (bob/yelling? question) "Whoa, chill out!"
+    (bob/question? question) "Sure."
+    :else "Whatever."
    )
   )
