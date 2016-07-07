0171773f28d614cd3dfc1db514cfa5cfbdc4cd85
Guilherme Moreira <guilherme@gmmoreira.com>
2016-07-07 10:14:03 -0300

    Better solution for 'robot-name' exercise

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -2,17 +2,14 @@
 
 (def ^:private allowed-numbers (range 0 10))
 (def ^:private allowed-letter (map char (range 65 91)))
-(def ^:private robot-list [])
 
 (defn- get-random-letter
   []
-  (rand-nth allowed-letter)
-  )
+  (rand-nth allowed-letter))
 
 (defn- get-random-number
   []
-  (rand-nth allowed-numbers)
-  )
+  (rand-nth allowed-numbers))
 
 (defn- generate-name
   []
@@ -20,40 +17,16 @@
        (get-random-letter)
        (get-random-number)
        (get-random-number)
-       (get-random-number)
-    )
-  )
-
-(defn- get-new-robot-id
-  []
-  (count robot-list)
-  )
-
-(defn- get-robot-by-id
-  [robot]
-  (robot-list (robot :id))
-  )
+       (get-random-number)))
 
 (defn robot
   []
-  (let [robot-id (get-new-robot-id)
-        new-robot { :id robot-id :name (generate-name) }
-        ]
-    (def robot-list (conj robot-list new-robot))
-    new-robot
-    )
-  )
+  (atom {:name (generate-name)}))
 
 (defn robot-name
   [robot]
-  ((get-robot-by-id robot) :name)
-  )
+  (:name @robot))
 
 (defn reset-name
   [robot]
-  (let [new-robot (assoc robot :name (generate-name))
-        ]
-    (def robot-list (assoc robot-list (robot :id) new-robot))
-    new-robot
-    )
-  )
+  (swap! robot assoc :name (generate-name)))
